e327592992d9b46c4f7e2fef4ddc1015e58c942f
nguyenanninh1212212 <ninh56085@gmail.com>
2023-08-28 01:08:34 +0700

    tp2

---
+++ template.js
@@ -0,0 +1,39 @@
+const userINP=document.getElementById("username")
+const passINP=document.getElementById("pass")
+const Click_to_sigh=document.getElementById("Click_to_sigh")
+
+const KEY="key"
+const datakey ={
+    data:localStorage.getItem(KEY) ? JSON.parse(localStorage.getItem(KEY)):[],
+    setData: function(newData){
+    this.data =newData
+    }
+    
+}
+
+Click_to_sigh.addEventListener("click",()=>{
+    const value_user=userINP.value;
+    const value_pass=passINP.value;
+       
+
+    if(!value_user && !value_pass) {
+        alert('chua nhap');
+        return;
+      } 
+   userINP.value="";
+   passINP.value ="";
+    const newData={
+        id:Date.toString(),
+        name:value_user,
+        pass:value_pass,
+    }
+    datakey.setData([...datakey.data,newData])
+    localStorage.setItem(KEY,JSON.stringify(datakey.data))
+    console.log(newData);
+})
+
+const userName = 'a'
+const password = 'a'
+
+const add= JSON.parse(localStorage.getItem(KEY))
+console.log(add.find((user) => user.name === userName && user.pass === password));      
